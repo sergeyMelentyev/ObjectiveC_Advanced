75cee69f040a1e078b5ba60eb155cd0bc75b026c
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-03-03 11:30:25 +0300

    new version

---
+++ ObjectiveC_Advanced/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectiveC_Advanced
+//
+//  Created by Админ on 03.03.16.
+//  Copyright © 2016 Melentyev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
