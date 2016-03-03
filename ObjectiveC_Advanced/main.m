04f04890092ce0ad4f8725df97d41dd62ffbaa66
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-03-03 11:39:50 +0300

    new version

--- ObjectiveC_Advanced/main.m
+++ ObjectiveC_Advanced/main.m
@@ -8,8 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
+// BLOCK STRUCTURE PASSED AS ARGUMENT
+void doSomethingWithTheBlock(void (^myBlock)(NSString *)) {
+    
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        
+        // BLOCK DEFINITION
+        void (^myBlock)(NSString *);
+        myBlock = ^(NSString *x) {
+            NSLog(@"%@", x);
+        };
+        doSomethingWithTheBlock(myBlock);
+        
+        
+        
+        
         
     }
     return 0;
